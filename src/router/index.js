11c96a9c7b9e11814781e8a4d9feb358f4f0167e
Sun.k <lovesunsixin@qq.com>
2019-12-25 20:59:24 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,24 @@ import VueRouter from 'vue-router'
 import Vue from 'vue'
 
 import Login from '@/views/login'
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
+import NotFound from '@/views/404'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
     // 登录
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/',
+      component: Home,
+      children: [
+      // 欢迎页面
+        { path: '/', component: Welcome }
+      ] },
+    // 通配以上规则没有的地址
+    { path: '*', component: NotFound }
   ]
 })
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,31 +3,73 @@
     <el-card class="my-card">
       <img src="../../assets/logo_index.png" alt />
       <!-- 表单 -->
-      <el-form :model="loginForm">
-        <el-form-item>
+      <el-form ref="loginForm" :model="loginForm" :rules="LoginRules">
+        <el-form-item prop="mobile">
           <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:238px;margin-right:10px"></el-input>
           <el-button>发送验证码</el-button>
         </el-form-item>
           <el-form-item>
         <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
-        <el-button type="primary" style="width:100%">登录</el-button>
+        <el-button type="primary" style="width:100%" @click="login()">登录</el-button>
       </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
+import store from '@/store'
 export default {
   data () {
+    // 校验手机号的函数
+    const checkMobile = (rule, value, callback) => {
+      // 自己的校验逻辑：1开头  第二位是3-9之间  最后是9个数字结尾
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        return callback(new Error('手机号格式不对'))
+      }
+      callback()
+    }
     return {
       loginForm: {
-        mobile: '',
-        code: ''
+        mobile: '139111111111',
+        code: '246810'
+      },
+      LoginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          // 自定义校验
+          { validate: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位验证码', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate((valid) => {
+        // valid判断是否校验成功
+        if (valid) {
+          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+            .then(res => {
+              // 登录成功
+              // res 是响应对象 res.data是响应主体
+              // 存储用户信息
+              store.setUser(res.data.data)
+              // 直接跳转首页
+              this.$router.push('/')
+            }).catch(e => {
+              // 登录失败
+              // e 错误对象
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
     }
   }
 }
